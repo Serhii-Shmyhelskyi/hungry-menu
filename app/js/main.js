ff447b8d55b5c37ef303467de3df840f24804f1e
Сергей Шмигельский <siro181a@gmail.com>
2020-11-25 16:30:42 +0200

    added content and slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,12 @@
 $(function () {
 
-
+    $('.specialties__inner').slick({
+        arrows: false,
+        dots: true,
+        dotsClass: "my-dots",
+        slidesToShow: 1,
+        slidesToScroll: 1
+    });
 
 
 })
